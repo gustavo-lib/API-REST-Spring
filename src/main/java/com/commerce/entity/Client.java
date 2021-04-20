95840390db9cbc1bee87cb441a7d0eef42d57b7a
gortiz <ggortiz09@gmail.com>
2021-04-19 23:43:00 -0300

    first commit

---
+++ src/main/java/com/commerce/entity/Client.java
@@ -0,0 +1,120 @@
+package com.commerce.entity;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+@Entity
+public class Client {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotEmpty
+	private String nombre;
+	
+	@NotEmpty
+	private String apellido;
+	
+	@NotEmpty
+	@Email
+	private String email;
+
+	@NotNull
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createAt;
+	
+	@OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Invoice> Invoices;
+	
+	public Client() {
+		Invoices = new ArrayList<Invoice>();
+	}
+
+	private String foto;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public List<Invoice> getInvoices() {
+		return Invoices;
+	}
+
+	public void setInvoices(List<Invoice> Invoices) {
+		this.Invoices = Invoices;
+	}
+	
+	public void addInvoice(Invoice Invoice) {
+		Invoices.add(Invoice);
+	}
+
+	@Override
+	public String toString() {
+		return nombre + " " + apellido;
+	}
+}
---
+++ src/main/java/com/commerce/entity/Invoice.java
@@ -0,0 +1,5 @@
+package com.commerce.entity;
+
+public class Invoice {
+
+}
