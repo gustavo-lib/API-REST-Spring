ac40ffabea199d0f09bf2d63d64826c4f82f9f58
gortiz <ggortiz09@gmail.com>
2021-04-20 00:03:18 -0300

    class invoice and client - bidirection

--- src/main/java/com/commerce/entity/Invoice.java
+++ src/main/java/com/commerce/entity/Invoice.java
@@ -1,5 +1,120 @@
 package com.commerce.entity;
 
-public class Invoice {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Invoice implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotEmpty
+	private String descripcion;
+
+	private String observacion;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "create_at")
+	private Date createAt;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Client Client;
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinColumn(name = "invoice_id")
+	private List<ItemInvoice> items;
+
+	public Invoice() {
+		this.items = new ArrayList<ItemInvoice>();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		createAt = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getObservacion() {
+		return observacion;
+	}
+
+	public void setObservacion(String observacion) {
+		this.observacion = observacion;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public Client getClient() {
+		return Client;
+	}
+
+	public void setClient(Client Client) {
+		this.Client = Client;
+	}
+
+	public List<ItemInvoice> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ItemInvoice> items) {
+		this.items = items;
+	}
+
+	public void addItemInvoice(ItemInvoice item) {
+		this.items.add(item);
+	}
+
+	public Double getTotal() {
+		Double total = 0.0;
+
+		int size = items.size();
+
+		for (int i = 0; i < size; i++) {
+			total += items.get(i).calcularImporte();
+		}
+		return total;
+	}
+
+	private static final long serialVersionUID = 1L;
 
 }
