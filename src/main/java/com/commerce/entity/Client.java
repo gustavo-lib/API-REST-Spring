aa7f574ec093ec872ec4ecaed18622fd6f9fde0d
gortiz <ggortiz09@gmail.com>
2021-04-20 12:41:25 -0300

    fix name fk

--- src/main/java/com/commerce/entity/Client.java
+++ src/main/java/com/commerce/entity/Client.java
@@ -18,8 +18,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-
-
 import org.springframework.format.annotation.DateTimeFormat;
 
 
