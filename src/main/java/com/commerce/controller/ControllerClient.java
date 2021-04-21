747d32259d364d4ef05d774720dcfc854b73c9c0
gortiz <ggortiz09@gmail.com>
2021-04-21 19:17:45 -0300

    bug

--- src/main/java/com/commerce/controller/ControllerClient.java
+++ src/main/java/com/commerce/controller/ControllerClient.java
@@ -1,10 +1,29 @@
 package com.commerce.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.commerce.entity.Client;
+import com.commerce.service.ServiceClient;
+
+
 
 @RestController
 @RequestMapping("/api/cliente")
 public class ControllerClient {
+	
+	
+	@Autowired
+	ServiceClient serviceClient;
+	@GetMapping("")
+	public List<Client> obtenerEmpleados(){
+		return serviceClient.findAll();
+		 
+	}
 
 }
--- src/main/java/com/commerce/service/impl/ServiceClientImpl.java
+++ src/main/java/com/commerce/service/impl/ServiceClientImpl.java
@@ -1,20 +1,30 @@
 package com.commerce.service.impl;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.commerce.entity.Client;
+import com.commerce.repository.IClient;
 import com.commerce.service.ServiceClient;
+
 
 @Service("clientImpl")
 public class ServiceClientImpl implements ServiceClient  {
 
+	@Autowired
+	IClient respositoryClient;
+	
 	@Override
 	public List<Client> findAll() {
 		// TODO Auto-generated method stub
-		return null;
+		return respositoryClient.findAll();
+		
 	}
 
 	@Override
