e9a2b2ac71f50601a622c12a3a56313c640e1c5e
gortiz <ggortiz09@gmail.com>
2021-04-21 23:53:42 -0300

    responseentity

--- src/main/java/com/commerce/controller/ControllerClient.java
+++ src/main/java/com/commerce/controller/ControllerClient.java
@@ -3,6 +3,7 @@ package com.commerce.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +22,16 @@ public class ControllerClient {
 	@Autowired
 	ServiceClient serviceClient;
 	@GetMapping("")
-	public List<Client> obtenerEmpleados(){
-		return serviceClient.findAll();
+	public ResponseEntity<?> obtenerEmpleados(){
+	    try {
+			return ResponseEntity.status(HttpStatus.OK).body(serviceClient.findAll());
+		} catch (Exception e) {
+			// TODO: handle exception
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
+		
 		 
 	}
+	
 
 }
